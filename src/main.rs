29b464902b1706c397c61414e309e1c8c2e70248
Simon Paitrault <contact@freyskeyd.fr>
2015-11-20 10:41:17 +0100

    up
    Signed-off-by: Simon Paitrault <contact@freyskeyd.fr>

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,7 @@ extern crate nlp;
 extern crate serde;
 extern crate serde_json;
 extern crate threadpool;
-
-use std::sync::{Arc, Mutex};
+use std::sync::{Arc};
 use threadpool::ThreadPool;
 use std::sync::mpsc::channel;
 use nlp::distance::levenshtein;
@@ -15,55 +14,58 @@ use nlp::phonetics::metaphone::metaphone;
 use iron::prelude::*;
 use iron::status;
 use router::{Router};
-use std::io::{self, BufReader};
+use std::io::{self};
 use std::io::prelude::*;
 use std::fs::File;
-
 use std::path::Path;
 
-fn filename_to_string<'a>(s: &'a str) -> String {
-    let path = Path::new(s);
-    let mut file = File::open(&path).unwrap();
-    let mut s = String::new();
-    file.read_to_string(&mut s);
-    s
-}
-
-fn words_by_line<'a>(s: &'a str) -> Vec<&'a str> {
-    s.lines().collect()
-}
+// Script de detection de mots
 fn main() {
+    // Recuperation du dico de mots
     let path = Path::new("words2.txt");
     let mut s = String::new();
     let mut file = File::open(&path).unwrap();
     file.read_to_string(&mut s).unwrap();
+    
+    // Création d'un Vector contenant la liste de mots
     let wbyl: Vec<String> = s.lines().map(|s| s.to_owned()).collect();
+    // Préproccessing de metaphone pour chaque mots dans le dico et création d'un Vec
     let v: Vec<(String, String)> = s.clone().lines().map(|s| s.to_owned()).map(|s| (s.to_owned(), metaphone(&s))).collect();
+    
     let meta_dict_arc = Arc::new(v);
     let v_arc = Arc::new(wbyl);
+    
+    // Init du router
     let mut router = Router::new();
 
+    // Handler de la request de test
+    // Le :query est le mot a tester contre le dico
     fn handler<'a>(req: &mut Request,  v: &'a Vec<String>, meta_dict: &'a Vec<(String, String)>) -> IronResult<Response> {
         let query = req.extensions.get::<Router>().unwrap().find("query").unwrap_or("/").to_owned();
+        
         let mut metaphone_result: Vec<&String> = Vec::new();
+        
         let v_arc = Arc::new(v.clone());
         let pool = ThreadPool::new(2);
-
         let (tx, rx) = channel();
-
         let arc_query = Arc::new(query.clone());
         let tx_lev = tx.clone();
 
+        // Création du metaphone de l'input
         let metaphone_input = metaphone(&query);
 
+        // Detection des matchs metaphone
         for i in meta_dict {
             if i.1 == metaphone_input {
                 metaphone_result.push(&i.0);
             }
         }
+        
         let query_lev = query.clone();
 
         let v_lev = v_arc.clone();
+        
+        // Création d'un thread pour l'execution du levenshtein algorithm
         pool.execute(move|| {
 
             for i in v_lev.iter() {
@@ -77,6 +79,8 @@ fn main() {
         let query_jaro = query.clone();
 
         let v_jaro = v_arc.clone();
+        
+        // Création d'un thread pour l'execution du jaro algorithm
         let jaro = pool.execute(move|| {
             let local_v = &v_jaro[..];
 
@@ -89,15 +93,16 @@ fn main() {
 
         let mut results: Vec<&str> = Vec::new();
 
+        // Recup des résultats des threads (ils sont vides pour le moment)
         for i in rx.iter().take(2) {
             println!("{:?}", i);
             results.extend(&i);
         }
+        // Création de la réponse JSON
         let serialized = serde_json::to_string(&metaphone_result).unwrap();
         Ok(Response::with((status::Ok, serialized)))
     };
 
     router.get("/:query", move |r: &mut Request| handler(r, &v_arc, &meta_dict_arc));
     Iron::new(router).http("localhost:3003").unwrap();
-
 }
